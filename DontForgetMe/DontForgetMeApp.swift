f281b256981b274304aebf54df235ad506bd206c
Michael <mikadifopadillaheredia@gmail.com>
2022-03-07 10:54:55 -0500

    Initial Commit

---
+++ DontForgetMe/DontForgetMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontForgetMeApp.swift
+//  DontForgetMe
+//
+//  Created by Michael Padilla on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct DontForgetMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
